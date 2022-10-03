7410212eb6d18142a7e324c6f5e99689310b2329
Niclas Geuken <niclas.fg@gmail.com>
2022-10-03 21:23:36 +0200

    add multiplication in the string evaluation

--- calculator.js
+++ calculator.js
@@ -13,6 +13,34 @@ function calculate(s){
     if (containsOnlyNumbers(s)) {
         return s;
     }
+    s = doMultiplication(s);
+    s = doAddition(s);
+    s = doSubtraction(s);
+    return s;
+}
+
+function doMultiplication(s){
+    while (s.includes('*')) {
+        let index = s.indexOf('*');
+        let beforeIndex = Math.max(s.slice(0,index).indexOf('+'), s.slice(0,index).indexOf('-'));
+        let nextPlusIndex = s.indexOf('+', index); 
+        let nextMinusIndex = s.indexOf('-',index);
+        if (nextPlusIndex == -1) {
+            nextPLusIndex = s.length;
+        }
+        if (nextMinusIndex == -1) {
+            nextMinusIndex = s.length;
+        }
+        let nextIndex = Math.min(nextPlusIndex, nextMinusIndex);
+        let s1 = calculate(s.slice(beforeIndex+1, index));
+        let s2 = calculate(s.slice(index+1,nextIndex));
+        let result = (parseInt(s1) * parseInt(s2));
+        s = s.slice(0,beforeIndex+1) + result.toString() + s.slice(nextIndex);
+    }
+    return s;
+}
+
+function doAddition(s) {
     if (s.includes('+')) {
         let index = s.indexOf('+');
         let s1 = calculate(s.slice(0, index));
@@ -20,6 +48,10 @@ function calculate(s){
         let result = (parseInt(s1)+parseInt(s2));
         return result.toString();
     }
+    return s;
+}
+
+function doSubtraction(s) {
     if (s.includes('-')) {
         let index = s.indexOf('-');
         let s1 = calculate(s.slice(0, index));
@@ -27,9 +59,8 @@ function calculate(s){
         let result = (parseInt(s1)-parseInt(s2));
         return result.toString();
     }
+    return s;
 }
 
-
-//let s = "10+25*2-11*(48:2)";
-let s = "58274+27857-27363";
+let s = "2*4+12";
 console.log("L31: " + calculate(s));
