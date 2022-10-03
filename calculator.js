6ff6464ceb0fa6dc7c65d38d986e2469026b0c24
Niclas Geuken <niclas.fg@gmail.com>
2022-10-03 19:39:43 +0200

    add function to split string into array

--- calculator.js
+++ calculator.js
@@ -1,1 +1,8 @@
-console.log("Hello Caluclator");
+
+// Takes a string s and returns an array of numbers, operators +-*:() and parenthesis.
+function splitString(s) {
+    return s.split(/([\+\*\-\:\(\)])/g).filter(Boolean);
+}
+
+let s = "10+25*2-11*(48:2)";
+console.log(splitString(s));
