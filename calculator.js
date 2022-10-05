193822a2f73ea99bdb32fdfb4d56c14745a3566e
Niclas Geuken <niclas.fg@gmail.com>
2022-10-05 20:24:06 +0200

    better parser for number-string

--- calculator.js
+++ calculator.js
@@ -8,59 +8,44 @@ function containsOnlyNumbers(str) {
     return /^\d+$/.test(str);
   }
 
+function add(a,b){
+    return a+b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function divide(a,b){
+    if(b == 0){ throw "You can't divide by zero."; }
+    return a/b;
+}
+
 function calculate(s){
     console.log("L8: " + s);
-    if (containsOnlyNumbers(s)) {
-        return s;
-    }
-    s = doMultiplication(s);
-    s = doAddition(s);
-    s = doSubtraction(s);
-    return s;
-}
-
-function doMultiplication(s){
-    while (s.includes('*')) {
-        let index = s.indexOf('*');
-        let beforeIndex = Math.max(s.slice(0,index).indexOf('+'), s.slice(0,index).indexOf('-'));
-        let nextPlusIndex = s.indexOf('+', index); 
-        let nextMinusIndex = s.indexOf('-',index);
-        if (nextPlusIndex == -1) {
-            nextPLusIndex = s.length;
+    let signs = ["*", "/", "+", "-"];
+    let funcs = [multiply, divide, add, subtract];
+    let tokens = s.split(/\b/);
+    for (let round = 0; round < signs.length; round++) {
+        console.log("L25: " + tokens.join(" "));          
+        for (let place = 0; place <tokens.length; place++) {
+            if (tokens[place] == signs[round]) {
+                let a = parseInt(tokens[place - 1]);
+                let b = parseInt(tokens[place + 1]);
+                let result = funcs[round](a,b);
+                tokens[place -1] = result.toString();
+                tokens.splice(place--, 2);
+            }
+            
         }
-        if (nextMinusIndex == -1) {
-            nextMinusIndex = s.length;
-        }
-        let nextIndex = Math.min(nextPlusIndex, nextMinusIndex);
-        let s1 = calculate(s.slice(beforeIndex+1, index));
-        let s2 = calculate(s.slice(index+1,nextIndex));
-        let result = (parseInt(s1) * parseInt(s2));
-        s = s.slice(0,beforeIndex+1) + result.toString() + s.slice(nextIndex);
     }
-    return s;
+    return tokens[0];
 }
 
-function doAddition(s) {
-    if (s.includes('+')) {
-        let index = s.indexOf('+');
-        let s1 = calculate(s.slice(0, index));
-        let s2 = calculate(s.slice(index+1));
-        let result = (parseInt(s1)+parseInt(s2));
-        return result.toString();
-    }
-    return s;
-}
 
-function doSubtraction(s) {
-    if (s.includes('-')) {
-        let index = s.indexOf('-');
-        let s1 = calculate(s.slice(0, index));
-        let s2 = calculate(s.slice(index+1));
-        let result = (parseInt(s1)-parseInt(s2));
-        return result.toString();
-    }
-    return s;
-}
-
-let s = "2*4+12";
+let s = "2*4+12-15/3";
 console.log("L31: " + calculate(s));
